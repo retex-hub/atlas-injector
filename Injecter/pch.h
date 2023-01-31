d991155308d31f823c01cdafcb5ae37da9469571
Retex <60952271+retex-hub@users.noreply.github.com>
2023-01-30 18:08:40 -0800

    Add files via upload

---
+++ Injecter/pch.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <iostream>
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <vector>
+#include <Psapi.h>
+#include <cstring>
+#include <array>
+#include <thread>
+#include <iterator>
+#include <math.h>
+#include <fstream>
+#include <strsafe.h>
+#include <vector>
+#include <iostream>
+#include <stdexcept>
+#include <filesystem>
+#include <stdio.h>
+#include <string>
+#include <filesystem>
+#include <fstream>
+#include <Windows.h>
+#include <tlhelp32.h>
+#include <thread>
+#include <filesystem>
+#include <iostream>
+#include <wininet.h>
+#include "md5.h"
+#include <iomanip>
+#include <limits>
+#include <iostream> // for standard I/O
+#include <string>   // for strings
+#include <iomanip>  // for controlling float print precision
+#include <sstream>  // string to number conversion
+
+#include <ctime>
+#include <future>
+#include <fstream>
+#include <string>
+#pragma comment(lib,"wininet")
+#pragma comment(lib, "urlmon.lib")
+
+
+using namespace std;
